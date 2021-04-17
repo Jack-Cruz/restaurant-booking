b6229a225c1f62c8f11e52fb04abfa773aea540b
Yefri <contact@yefri.com>
2021-04-16 23:24:05 -0500

    Initial Project

---
+++ backend/src/main/java/com/restaurantbookingapi/RestaurantBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantbookingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBookingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantBookingApiApplication.class, args);
+    }
+
+}
